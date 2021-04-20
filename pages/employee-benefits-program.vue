1f6dc25a1057050c41c6383ce4a4149e3e779975
Pawan Kumar <surpawan@gmail.com>
2021-04-20 23:08:17 +0530

    Added Pages

---
+++ pages/employee-benefits-program.vue
@@ -0,0 +1,204 @@
+<template>
+  <div>
+    <section class="text-white relative">
+      <div
+        class="min-h-screen hero-image bg-center bg-cover flex"
+        :style="mainHeaderImage"
+      >
+        <div class="relative container mx-auto p-4 flex items-center z-10">
+          <div>
+            <div class="content float-left py-4 px-5 my-5">
+              <div
+                class="heading mb-3 tracking-wide uppercase underline max-w-xl font-bold text-2xl md:text-4xl"
+              >
+                OFFER OUR SERVICES AS AN AMAZING BENEFIT FOR YOUR STAFF
+              </div>
+              <div
+                class="text-lg font-light leading-normal hidden max-w-md sm:block"
+              >
+                It costs nothing for your organization to partner with us, and
+                provides your staff with a one-of-a-kind benefit.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-black pt-16">
+      <div
+        class="flex flex-col place-items-center justify-center mx-auto max-w-4xl"
+      >
+        <div class="text-brand uppercase text-lg mb-3">
+          EAB SERVICE AS A BENEFIT
+        </div>
+        <div class="text-white text-3xl font-semibold uppercase mb-5">
+          EXCLUSIVE AUTO PURCHASING PROGRAM
+        </div>
+        <div class="text-gray-300 pb-24">
+          We partner with your organization for free so you may offer an
+          auto-purchasing program as part of your benefits package. By doing so
+          you give your staff access to utilize our unique cost-free vehicle
+          purchasing service, allowing them to find their perfect car while
+          saving time and money. It is a win-win!
+        </div>
+        <div class="uppercase text-brand mb-4">Learn more</div>
+        <a
+          href="#aboutEmployeeBenefitsProgram"
+          class="rounded-full h-12 w-12 flex items-center justify-center -mb-5 bg-brand-gray-dark"
+        >
+          <fa class="text-brand text-2xl" :icon="faChevronDown" />
+        </a>
+      </div>
+    </section>
+    <section
+      id="aboutEmployeeBenefitsProgram"
+      class="bg-white text-black py-16"
+    >
+      <div class="container mx-auto py-16">
+        <div class="grid grid-cols-10 my-10 gap-4">
+          <div class="col-span-1">
+            <fa class="text-8xl text-brand" :icon="faBuilding" />
+          </div>
+          <div class="col-span-3 text-3xl font-semibold">
+            HOW IT WORKS FOR YOUR ORGANIZATION
+          </div>
+          <div></div>
+          <div class="col-span-5">
+            <div class="mb-4">
+              Getting set-up with us is simple. We setup a personalized link for
+              your staff to utilize if they are interested in our service.
+              Depending on the size of your organization we may assign a
+              dedicated Executive Concierge or a team to spefically handle your
+              account.
+            </div>
+            <div class="mb-4">
+              We then work with your HR department to provide any collateral or
+              materials your organization deems neccessary. If you currently
+              work with a company that handles a benefit portal for you, we will
+              work with them to get your material implemented.
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div class="grid grid-cols-10 my-10 gap-4">
+          <div class="col-span-1">
+            <fa class="text-8xl text-brand" :icon="faMobile" />
+          </div>
+          <div class="col-span-3 text-3xl font-semibold">
+            HOW IT WORKS FOR YOUR STAFF
+          </div>
+          <div></div>
+          <div class="col-span-5">
+            <div class="mb-4">
+              Your staff simply utilizes the link we have provided for your
+              organization to learn more about our service, or if they are ready
+              to make a purchase submit a vehicle request. That’s it! We take
+              over from there.
+            </div>
+            <div class="mb-4">
+              We help your staff find the perfect vehicle they are searching
+              for, get them the best vehicle for the best price, handle all of
+              their neccessary paperwork, and have it delivered wherever they
+              want - all without any cost to them!
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div class="grid grid-cols-10 my-10 gap-4">
+          <div class="col-span-1">
+            <fa class="text-8xl text-brand" :icon="faHandHoldingUsd" />
+          </div>
+          <div class="col-span-3 text-3xl font-semibold">
+            YOU EARN WHEN WE EARN
+          </div>
+          <div></div>
+          <div class="col-span-5">
+            <div class="mb-4">
+              On top of setting up and giving access to our service to your
+              staff free of cost, we also make sure that your organization
+              financially benefits from our service. Anytime a member of your
+              staff purchases a vehicle through our service, you will recieve a
+              pay-out from us.
+              <span class="text-brand">
+                { ' That’s right, we are a free benefit for your organization
+                that actually pays you! ' }
+              </span>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div class="grid grid-cols-10 my-10 gap-4">
+          <div class="col-span-1">
+            <fa class="text-8xl text-brand" :icon="faGraduationCap" />
+          </div>
+          <div class="col-span-3 text-3xl font-semibold">
+            VEHICLE PURCHASING EDUCATION
+          </div>
+          <div></div>
+          <div class="col-span-5">
+            <div class="mb-4">
+              One of the passions that drives our business is a love for
+              educating and helping people not only getting the best deal
+              possible on a vehicle, but also making sure they are making sound
+              decisions and understand the process.
+            </div>
+            <div class="mb-4">
+              If you need additional resources or would like our help in
+              providing vehicle purchasing education for your staff we are
+              available. Whether that means attending a staff event, hosting a
+              zoom call, or just answering questions your staff has - we can
+              handle it.
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-brand-grayAlternate text-white py-16">
+      <CallToAction
+        title="Ready to get started?"
+        subtitle="START YOUR VEHICLE REQUEST"
+        button-text="Get Started"
+      />
+    </section>
+  </div>
+</template>
+
+<script>
+import bgMainHeader from 'assets/images/employee_benefits_header.jpg'
+import {
+  faBuilding,
+  faChevronDown,
+  faGraduationCap,
+  faHandHoldingUsd,
+  faMobile,
+} from '@fortawesome/free-solid-svg-icons'
+import CallToAction from '~/components/CallToAction'
+export default {
+  transitions: 'fade',
+  components: {
+    CallToAction,
+  },
+  data() {
+    return {
+      mainHeaderImage: { backgroundImage: `url(${bgMainHeader})` },
+    }
+  },
+  computed: {
+    faBuilding() {
+      return faBuilding
+    },
+    faChevronDown() {
+      return faChevronDown
+    },
+    faGraduationCap() {
+      return faGraduationCap
+    },
+    faHandHoldingUsd() {
+      return faHandHoldingUsd
+    },
+    faMobile() {
+      return faMobile
+    },
+  },
+}
+</script>
---
+++ pages/friends-and-family-program.vue
@@ -0,0 +1,215 @@
+<template>
+  <div>
+    <section class="text-white relative">
+      <div
+        class="min-h-screen hero-image bg-center bg-cover flex"
+        :style="mainHeaderImage"
+      >
+        <div class="relative container mx-48 p-4 flex items-center z-10">
+          <div>
+            <div class="content float-left py-4 px-5 my-5">
+              <div
+                class="heading mb-3 tracking-wide uppercase underline max-w-lg font-bold text-2xl md:text-4xl"
+              >
+                SHARE YOUR EXCITEMENT WITH YOUR FRIENDS & FAMILY!
+              </div>
+              <div class="text-lg font-light leading-normal max-w-md block">
+                Spread the word so we can help more people experience the EAB
+                difference while finding their perfect vehicle!
+              </div>
+              <div class="grid grid-cols-5 text-white mt-4 gap-4 w-max-xl">
+                <div class="text-center">
+                  <fa class="text-6xl mb-4" :icon="faBullhorn" />
+                  <h1 class="text-xl font-semibold">SHARE</h1>
+                </div>
+                <div class="pt-10">
+                  <hr class="border-gray-400 border-t-2" />
+                </div>
+                <div class="text-center">
+                  <fa class="text-6xl mb-4" :icon="faCommentAlt" />
+                  <h1 class="text-xl font-semibold">EDUCATE</h1>
+                </div>
+                <div class="pt-10">
+                  <hr class="border-gray-400 border-t-2" />
+                </div>
+                <div class="text-center">
+                  <fa class="text-6xl mb-4" :icon="faMoneyBill" />
+                  <h1 class="text-xl font-semibold">EARN</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-black pt-16">
+      <div
+        class="flex flex-col place-items-center justify-center mx-auto max-w-4xl"
+      >
+        <div class="text-brand uppercase text-lg mb-3">LOVE OUR SERVICE?</div>
+        <div class="text-white text-3xl font-semibold uppercase mb-5">
+          EARN REWARDS FOR SHARING YOUR EXPERIENCE
+        </div>
+        <div class="text-gray-300 pb-24">
+          Our Friends & Family Program allows you to earn rewards for telling
+          your friends and family about our service. If they submit a request
+          and purchase a vehicle they just need to enter your Friends & Family
+          Code during their submission and you get rewarded!
+        </div>
+        <div class="uppercase text-brand mb-4">Learn more</div>
+        <a
+          href="#aboutFriendsAndFamilyProgram"
+          class="rounded-full h-12 w-12 flex items-center justify-center -mb-5 bg-brand-gray-dark"
+        >
+          <fa class="text-brand text-2xl" :icon="faChevronDown" />
+        </a>
+      </div>
+    </section>
+    <section
+      id="aboutFriendsAndFamilyProgram"
+      class="bg-white text-black py-16"
+    >
+      <div class="container mx-auto py-16">
+        <div class="grid grid-cols-10 my-10 gap-4">
+          <div class="col-span-1">
+            <fa class="text-8xl text-brand" :icon="faUsers" />
+          </div>
+          <div class="col-span-3 text-3xl font-semibold">
+            GETTING YOUR FRIENDS & FAMILY CODE
+          </div>
+          <div></div>
+          <div class="col-span-5">
+            <div class="mb-4">
+              After purchasing a vehicle from us all clients are given a unique
+              Friends & Family code. This code is your personal identifier that
+              allows us to track any vehicle requests that come in that belong
+              to your friends & family.
+            </div>
+            <div class="mb-4">
+              If you have not purchased a vehicle from us but are still
+              interested in spreading the word to your friends & family please
+              fill out the below form so we can get you signed up.
+            </div>
+            <div class="mb-4">
+              <button
+                class="bg-brand shadow-lg py-2 px-8 rounded uppercase text-white mb-0.5"
+              >
+                Sign Up
+                <fa class="ml-2 text-white" :icon="faChevronRight" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div class="grid grid-cols-10 my-10 gap-4">
+          <div class="col-span-1">
+            <fa class="text-8xl text-brand" :icon="faCcVisa" />
+          </div>
+          <div class="col-span-3 text-3xl font-semibold">
+            YOUR FRIENDS & FAMILY REWARDS
+          </div>
+          <div></div>
+          <div class="col-span-5">
+            <div class="mb-4">
+              Friends & Family Program members will
+              <span class="text-yellow-500">
+                {{ ' earn a minimum of $100 and a maximum of $200 ' }}
+              </span>
+              for each time their code is used for a vehicle purchase.
+            </div>
+            <div class="mb-4">
+              Rewards will be sent out in a prepaid Visa gift card in the month
+              following when the registered Friends & Family vehicle is
+              purchased (Example: Your code is used in March - You will recieve
+              your gift card in April).
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div class="grid grid-cols-10 my-10 gap-4">
+          <div class="col-span-1">
+            <fa class="text-8xl text-brand" :icon="faHandsHelping" />
+          </div>
+          <div class="col-span-3 text-3xl font-semibold">
+            WANT TO EARN MORE?
+          </div>
+          <div></div>
+          <div class="col-span-5">
+            <div class="mb-4">
+              Have a lot of friends & family or a large network? Have a passion
+              for automobiles? Think you would enjoy assisting people in having
+              the best car buying experience they’ve ever had?
+            </div>
+            <div class="mb-4">
+              If you answered yes to all of these questions you may be a good
+              fit for joining the ExecutiveAB team as an Executive Concierge.
+              <NuxtLink to="/careers" class="ml-1 text-brand underline">
+                {{ 'Visit our careers page ' }}
+              </NuxtLink>
+              for more information. We are always looking for people like you.
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="bg-brand-grayAlternate text-white py-16">
+      <CallToAction
+        title="Ready to get started?"
+        subtitle="START YOUR VEHICLE REQUEST"
+        button-text="Get Started"
+      />
+    </section>
+  </div>
+</template>
+
+<script>
+import bgMainHeader from 'assets/images/friends_and_family_header.jpg'
+import {
+  faBullhorn,
+  faChevronDown,
+  faChevronRight,
+  faCommentAlt,
+  faHandsHelping,
+  faMoneyBill,
+  faUsers,
+} from '@fortawesome/free-solid-svg-icons'
+import { faCcVisa } from '@fortawesome/free-brands-svg-icons'
+import CallToAction from '~/components/CallToAction'
+export default {
+  transitions: 'fade',
+  components: {
+    CallToAction,
+  },
+  data() {
+    return {
+      mainHeaderImage: { backgroundImage: `url(${bgMainHeader})` },
+    }
+  },
+  computed: {
+    faBullhorn() {
+      return faBullhorn
+    },
+    faChevronDown() {
+      return faChevronDown
+    },
+    faChevronRight() {
+      return faChevronRight
+    },
+    faCommentAlt() {
+      return faCommentAlt
+    },
+    faHandsHelping() {
+      return faHandsHelping
+    },
+    faMoneyBill() {
+      return faMoneyBill
+    },
+    faUsers() {
+      return faUsers
+    },
+    faCcVisa() {
+      return faCcVisa
+    },
+  },
+}
+</script>
