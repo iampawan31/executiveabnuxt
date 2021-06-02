95ae5e445b631122aa88d7f088899a99afdd742e
Pawan Kumar <surpawan@gmail.com>
2021-06-02 14:16:42 +0530

    Removed Github pages feature

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,4 @@
 export default {
-  target: 'static',
-  router: {
-    base: '/executiveabnuxt/',
-  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Executive AB',
