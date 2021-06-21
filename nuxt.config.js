02a5878d122b3152bdb769ec07edb8344278cbbb
Pawan Kumar <surpawan@gmail.com>
2021-06-22 00:45:55 +0530

    Added Static

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
   head: {
     title: 'Executive AB',
     htmlAttrs: {
